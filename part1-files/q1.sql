711fb981cd086ce57c147650a31b93f13b2a49d0
Katherine Huang <kjhuang523@gmail.com>
2017-03-05 14:54:30 -0500

    query 1 code complete - will debug

--- part1-files/q1.sql
+++ part1-files/q1.sql
@@ -19,6 +19,54 @@ DROP VIEW IF EXISTS intermediate_step CASCADE;
 
 -- Define views for your intermediate steps here.
 
+-- find # of grades that are between 80 and 100 for each assignment
+CREATE VIEW NUM_80_100(
+	select count(AssignmentGroup.group_id)
+	from Grade outer join AssignmentGroup on group_id
+	where Grade.grade >= 80 and Grade.grade <= 100
+	group by AssignmentGroup.assignment_id;
+);
+
+-- find # of grades that are between 60 and 79 for each assignment
+CREATE VIEW NUM_60_79(
+	select count(AssignmentGroup.group_id)
+	from Grade outer join AssignmentGroup on group_id
+	where Grade.grade >= 60 and Grade.grade <= 79
+	group by AssignmentGroup.assignment_id;
+);
+
+-- find # of grades that are between 50 and 59 for each assignment
+CREATE VIEW NUM_50_59(
+	select count(AssignmentGroup.group_id)
+	from Grade outer join AssignmentGroup on group_id
+	where Grade.grade >= 50 and Grade.grade <= 59
+	group by AssignmentGroup.assignment_id;
+);
+
+-- find # of grades that are between 0 and 49 for each assignment
+CREATE VIEW NUM_0_49(
+	select count(AssignmentGroup.group_id)
+	from Grade outer join AssignmentGroup on group_id
+	where Grade.grade >= 0 and Grade.grade <= 49
+	group by AssignmentGroup.assignment_id;
+);
+
+-- find # of grades that are between 50 and 59 for each assignment
+CREATE VIEW AVG(
+	select avg(Grade.grade)
+	from Grade, AssignmentGroup
+	where Grade.group_id = AssignmentGroup.group_id
+	group by AssignmentGroup.assignment_id;
+);
+
+CREATE VIEW ID(
+	select AssignmentGroup.assignment_id
+	from Grade outer join AssignmentGroup on group_id
+	group by AssignmentGroup.assignment_id;
+);
+
+
 -- Final answer.
-INSERT INTO q1 
+INSERT INTO q1 values
 	-- put a final query here so that its results will go into the table.
+	(ID, AVG, NUM_80_100, NUM_60_79, NUM_50_59, NUM_0_49);
