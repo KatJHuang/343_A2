77f48468af578aefec533ce2ccdf6bc58f768fd8
robin Li <robinlee09201@gmail.com>
2017-03-12 23:30:19 -0400

    jdbc finished

--- Assignment2.java
+++ Assignment2.java
@@ -1,6 +1,6 @@
 import java.sql.*;
 
-// Remember that part of your mark is for doing as much in SQL (not Java) 
+// Remember that part of your mark is for doing as much in SQL (not Java)
 // as you can. At most you can justify using an array, or the more flexible
 // ArrayList. Don't go crazy with it, though. You need it rarely if at all.
 import java.util.ArrayList;
@@ -20,11 +20,11 @@ public class Assignment2 {
 
     /**
      * Connects to the database and sets the search path.
-     * 
+     *
      * Establishes a connection to be used for this session, assigning it to the
      * instance variable 'connection'. In addition, sets the search path to
      * markus.
-     * 
+     *
      * @param url
      *            the url for the database
      * @param username
@@ -34,27 +34,41 @@ public class Assignment2 {
      * @return true if connecting is successful, false otherwise
      */
     public boolean connectDB(String URL, String username, String password) {
+		try {
+
+			connection = DriverManager.getConnection(URL, username, password);
+			String q = "SET search_path to markus;";
+			PreparedStatement pS = connection.prepareStatement(q);
+			pS.executeUpdate();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
         // Replace this return statement with an implementation of this method!
-        return false;
+        return true;
     }
 
     /**
      * Closes the database connection.
-     * 
+     *
      * @return true if the closing was successful, false otherwise
      */
     public boolean disconnectDB() {
+		try {
+			connection.close();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
         // Replace this return statement with an implementation of this method!
-        return false;
+        return true;
     }
 
     /**
      * Assigns a grader for a group for an assignment.
-     * 
+     *
      * Returns false if the groupID does not exist in the AssignmentGroup table,
      * if some grader has already been assigned to the group, or if grader is
      * not either a TA or instructor.
-     * 
+     *
      * @param groupID
      *            id of the group
      * @param grader
@@ -63,21 +77,76 @@ public class Assignment2 {
      */
     public boolean assignGrader(int groupID, String grader) {
         // Replace this return statement with an implementation of this method!
-        return false;
+        String q;
+        ResultSet rs;
+        PreparedStatement pS;
+		try {
+			
+			q = "select group_id from AssignmentGroup";
+			pS = connection.prepareStatement(q);
+			rs = pS.executeQuery();
+
+			boolean valid = false;
+			while(rs.next()){
+			  if(rs.getInt("group_id")==groupID){
+				valid = true;
+				break;
+			  }
+			}
+			if(!valid){
+			  return false;
+			}
+			valid = false;
+			q = "select username from MarkusUser where type = 'instructor' or type = 'TA'";
+			pS = connection.prepareStatement(q);
+			rs = pS.executeQuery();
+
+			while(rs.next()){
+			  if(rs.getString("username").equals(grader)){
+				valid = true;
+				break;
+			  }
+			}
+			if(!valid){
+			  return false;
+			}
+			
+			//already in grader
+			q = "select * from Grader";
+			pS = connection.prepareStatement(q);
+			rs = pS.executeQuery();
+
+			while(rs.next()){
+			  if(rs.getInt("group_id")==groupID && rs.getString("username").equals(grader)){
+				return false;
+			  }
+			}
+			//INSERT
+
+			q = "insert into Grader VALUES (?, ?)";
+			pS = connection.prepareStatement(q);
+			pS.setInt(1,groupID);
+			pS.setString(2,grader);
+			pS.executeUpdate();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+			return false;
+		}
+        return true;
     }
 
     /**
      * Adds a member to a group for an assignment.
-     * 
+     *
      * Records the fact that a new member is part of a group for an assignment.
      * Does nothing (but returns true) if the member is already declared to be
      * in the group.
-     * 
+     *
      * Does nothing and returns false if any of these conditions hold: - the
      * group is already at capacity, - newMember is not a valid username or is
      * not a student, - there is no assignment with this assignment ID, or - the
      * group ID has not been declared for the assignment.
-     * 
+     *
      * @param assignmentID
      *            id of the assignment
      * @param groupID
@@ -88,40 +157,103 @@ public class Assignment2 {
      */
     public boolean recordMember(int assignmentID, int groupID, String newMember) {
         // Replace this return statement with an implementation of this method!
-        return false;
+        boolean valid = false;
+        String q;
+        ResultSet rs;
+        PreparedStatement pS;
+		
+		try {
+			//check member already in
+			q = "select * from Membership";
+			pS = connection.prepareStatement(q);
+			rs = pS.executeQuery();
+
+			while(rs.next()){
+			  if(rs.getString("username").equals(newMember) && rs.getInt("group_id")==groupID){
+				  
+				return true;
+			  }
+			}
+			//check memebr
+			q = "select username from MarkusUser where type = 'student'";
+			pS = connection.prepareStatement(q);
+			rs = pS.executeQuery();
+
+			while(rs.next()){
+			  if(rs.getString("username").equals(newMember)){
+				valid = true;
+				break;
+			  }
+			}
+			if(!valid){
+			  return false;
+			}
+			valid = false;
+
+			//check assignment and group
+			q = "select assignment_id, group_id from"+
+			" Assignment natural join AssignmentGroup natural join Membership"+
+			" group by group_id, assignment_id having count(username) < group_max;";
+			pS = connection.prepareStatement(q);
+			rs = pS.executeQuery();
+
+			while(rs.next()){
+			  if(rs.getInt("group_id")==groupID && rs.getInt("assignment_id")==assignmentID){
+				valid = true;
+				break;
+			  }
+			}
+			if(!valid){
+			  return false;
+			}
+			
+
+
+			//INSERT
+			q = "insert into Membership VALUES (?, ?)";
+			pS = connection.prepareStatement(q);
+			pS.setString(1,newMember);
+			pS.setInt(2,groupID);
+			pS.executeUpdate();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+			return false;
+		}
+
+        return true;
     }
 
     /**
      * Creates student groups for an assignment.
-     * 
+     *
      * Finds all students who are defined in the Users table and puts each of
      * them into a group for the assignment. Suppose there are n. Each group
      * will be of the maximum size allowed for the assignment (call that k),
      * except for possibly one group of smaller size if n is not divisible by k.
      * Note that k may be as low as 1.
-     * 
+     *
      * The choice of which students to put together is based on their grades on
      * another assignment, as recorded in table Results. Starting from the
      * highest grade on that other assignment, the top k students go into one
      * group, then the next k students go into the next, and so on. The last n %
      * k students form a smaller group.
-     * 
+     *
      * In the extreme case that there are no students, does nothing and returns
      * true.
-     * 
+     *
      * Students with no grade recorded for the other assignment come at the
      * bottom of the list, after students who received zero. When there is a tie
      * for grade (or non-grade) on the other assignment, takes students in order
      * by username, using alphabetical order from A to Z.
-     * 
+     *
      * When a group is created, its group ID is generated automatically because
      * the group_id attribute of table AssignmentGroup is of type SERIAL. The
      * value of attribute repo is repoPrefix + "/group_" + group_id
-     * 
+     *
      * Does nothing and returns false if there is no assignment with ID
      * assignmentToGroup or no assignment with ID otherAssignment, or if any
      * group has already been defined for this assignment.
-     * 
+     *
      * @param assignmentToGroup
      *            the assignment ID of the assignment for which groups are to be
      *            created
@@ -135,11 +267,137 @@ public class Assignment2 {
     public boolean createGroups(int assignmentToGroup, int otherAssignment,
             String repoPrefix) {
         // Replace this return statement with an implementation of this method!
-        return false;
+		if(assignmentToGroup == otherAssignment)
+			return false;
+
+		String q;
+        ResultSet rs;
+        PreparedStatement pS;
+		
+		
+		//no assignment
+		boolean as, oth_as;
+		as = false;
+		oth_as = false;
+
+		try{
+			
+			q = "select assignment_id from Assignment";
+			pS = connection.prepareStatement(q);
+			rs = pS.executeQuery();
+
+			while(rs.next()){
+			  if(rs.getInt("assignment_id")==assignmentToGroup){
+				as = true;
+			  }
+			  if(rs.getInt("assignment_id")==otherAssignment){
+				oth_as = true;
+			  }
+			}
+			if(!(as&&oth_as)){
+			  return false;
+			}
+			
+			//assignment has group
+			q = "select assignment_id from AssignmentGroup";
+			pS = connection.prepareStatement(q);
+			rs = pS.executeQuery();
+
+			while(rs.next()){
+			  if(rs.getInt("assignment_id")==assignmentToGroup){
+				return false;
+			  }
+			}
+			
+			//insert
+			
+			//determine k
+			q = "select group_max from Assignment where assignment_id = ?";
+			pS = connection.prepareStatement(q);
+			pS.setInt(1,assignmentToGroup);
+			rs = pS.executeQuery();
+			rs.next();
+			int k = rs.getInt("group_max");
+			
+			//determine the start of group_id
+			q = "select max(group_id) from AssignmentGroup";
+			pS = connection.prepareStatement(q);
+			rs = pS.executeQuery();
+			rs.next();
+			int groupID = rs.getInt("max");
+			
+			
+			q = "DROP VIEW IF EXISTS students CASCADE";
+			pS = connection.prepareStatement(q);
+			pS.executeUpdate();
+			q = "DROP VIEW IF EXISTS oth_group CASCADE";
+			pS = connection.prepareStatement(q);
+			pS.executeUpdate();
+			q = "DROP VIEW IF EXISTS student_mark CASCADE";
+			pS = connection.prepareStatement(q);
+			pS.executeUpdate();
+			q = "create view students as select username from MarkusUser where type = 'student'";
+			pS = connection.prepareStatement(q);
+			pS.executeUpdate();
+			q = "create view oth_group as select group_id from AssignmentGroup where assignment_id = " + otherAssignment;
+			pS = connection.prepareStatement(q);
+			pS.executeUpdate();
+			q = "create view student_mark as select students.username, mark from ";
+			q = q + "(oth_group natural join Membership natural join Result) FULL JOIN students on Membership.username = students.username;";
+			pS = connection.prepareStatement(q);
+			pS.executeUpdate();
+			q = "select username from student_mark order by mark DESC NULLS LAST, username ASC";
+			pS = connection.prepareStatement(q);
+			rs = pS.executeQuery();
+			
+			int i = 0;
+			while(rs.next()){
+				if(i==0){
+					//insert into AssignmentGroup
+					groupID++;
+					q = "insert into AssignmentGroup VALUES (? ,? ,?)";
+					pS = connection.prepareStatement(q);
+					pS.setInt(1,groupID);
+					pS.setInt(2,assignmentToGroup);
+					pS.setString(3,repoPrefix + "/group_" + groupID);
+					pS.executeUpdate();
+				}
+				
+				//insert into Membership
+				q = "insert into Membership VALUES (? ,?)";
+				pS = connection.prepareStatement(q);
+				pS.setString(1,rs.getString("username"));
+				pS.setInt(2,groupID); 
+				pS.executeUpdate();
+				
+				i++;
+				if(i>=k){
+					i=0;
+				}
+			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+			return false;
+		}
+		
+        return true;
     }
 
     public static void main(String[] args) {
         // You can put testing code in here. It will not affect our autotester.
-        System.out.println("Boo!");
+		try{
+			Assignment2 xx = new Assignment2();
+			String url = "jdbc:postgresql://localhost:5432/csc343h-liruibin";
+			xx.connectDB(url,"liruibin","");
+			
+			boolean asgd = xx.assignGrader(5,"t1");
+			boolean recm = xx.recordMember(2,2,"E");
+			boolean cg = xx.createGroups(6,1,"pre");
+			System.out.println("res:" + asgd + recm + cg);
+		}
+		catch (SQLException ex){
+			ex.printStackTrace();
+		}
+		
     }
 }
--- data.sql
+++ data.sql
@@ -16,10 +16,10 @@ TRUNCATE TABLE MarkusUser CASCADE;
 
 INSERT INTO MarkusUser VALUES ('i1', 'iln1', 'ifn1', 'instructor');
 
-INSERT INTO MarkusUser VALUES ('s1', 'sln1', 'sfn1', 'student');
-INSERT INTO MarkusUser VALUES ('s2', 'sln2', 'sfn2', 'student'); --
-INSERT INTO MarkusUser VALUES ('s3', 'sln3', 'sfn3', 'student');
-INSERT INTO MarkusUser VALUES ('s4', 'sln4', 'sfn4', 'student'); --
+INSERT INTO MarkusUser VALUES ('A', 'sln1', 'sfn1', 'student');
+INSERT INTO MarkusUser VALUES ('B', 'sln2', 'sfn2', 'student'); --
+INSERT INTO MarkusUser VALUES ('C', 'sln3', 'sfn3', 'student');
+INSERT INTO MarkusUser VALUES ('D', 'sln4', 'sfn4', 'student'), ('E', 'sln4', 'sfn4', 'student'); --
 
 INSERT INTO MarkusUser VALUES ('t1', 'tln1', 'tfn1', 'TA');
 INSERT INTO MarkusUser VALUES ('t2', 'tln2', 'tln2', 'TA');
@@ -27,38 +27,32 @@ INSERT INTO MarkusUser VALUES ('t3', 'tln3', 'tln3', 'TA');
 INSERT INTO MarkusUser VALUES ('t4', 'tln4', 'tln4', 'TA');
 
 -- assn id, descp, due, min, max
-INSERT INTO Assignment VALUES (1000, 'a1', '2017-01-08 20:00', 1, 2); --
-INSERT INTO Assignment VALUES (2000, 'a2', '2017-02-08 20:00', 1, 2);
-INSERT INTO Assignment VALUES (3000, 'a3', '2017-03-08 20:00', 1, 2);
-INSERT INTO Assignment VALUES (4000, 'a4', '2017-04-08 20:00', 1, 2);
-INSERT INTO Assignment VALUES (5000, 'a5', '2017-05-08 20:00', 1, 2);
+INSERT INTO Assignment VALUES (1, 'a1', '2017-01-08 20:00', 1, 2); --
+INSERT INTO Assignment VALUES (2, 'a2', '2017-02-08 20:00', 1, 2);
+INSERT INTO Assignment VALUES (3, 'a3', '2017-03-08 20:00', 1, 2);
+INSERT INTO Assignment VALUES (4, 'a4', '2017-04-08 20:00', 1, 2);
+INSERT INTO Assignment VALUES (5, 'a5', '2017-05-08 20:00', 1, 2);
+INSERT INTO Assignment VALUES (6, 'a6', '2017-05-08 20:00', 1, 2);
 
-INSERT INTO Required VALUES (1000, 'A1.pdf');
+INSERT INTO Required VALUES (1, 'A1.pdf');
 
 -- group id, assignment id
-INSERT INTO AssignmentGroup VALUES (1, 1000, 'repo_url');--
-INSERT INTO AssignmentGroup VALUES (4, 1000, 'repo_url');--
-INSERT INTO AssignmentGroup VALUES (2, 2000, 'repo_url');
-INSERT INTO AssignmentGroup VALUES (3, 3000, 'repo_url');
+INSERT INTO AssignmentGroup VALUES (1, 1, 'repo_url');--
+INSERT INTO AssignmentGroup VALUES (4, 1, 'repo_url');--
+INSERT INTO AssignmentGroup VALUES (2, 2, 'repo_url');
+INSERT INTO AssignmentGroup VALUES (3, 2, 'repo_url'),(5, 3, 'repo_url');
 
 -- student, group
-INSERT INTO Membership VALUES ('s1', 1);
-INSERT INTO Membership VALUES ('s2', 1);--
-INSERT INTO Membership VALUES ('s1', 2);
-INSERT INTO Membership VALUES ('s2', 2);--
-INSERT INTO Membership VALUES ('s3', 3);
-INSERT INTO Membership VALUES ('s4', 3);
-INSERT INTO Membership VALUES ('s3', 4);
-INSERT INTO Membership VALUES ('s4', 4);
+INSERT INTO Membership VALUES ('A', 1),('B',1),('C',2),('D',3),('E',4);
 
 -- sub id, group id
-INSERT INTO Submissions VALUES (11, 'A1.pdf', 's1', 1, '2017-02-08 19:59');
-INSERT INTO Submissions VALUES (21, 'A1.pdf', 's2', 1, '2017-02-06 19:59');
-INSERT INTO Submissions VALUES (31, 'A1.pdf', 's1', 1, '2017-02-05 19:59');
-INSERT INTO Submissions VALUES (41, 'A1.pdf', 's2', 1, '2017-02-04 19:59');
-INSERT INTO Submissions VALUES (51, 'A1.pdf', 's1', 1, '2017-02-03 19:59');
-INSERT INTO Submissions VALUES (61, 'A1.pdf', 's3', 4, '2017-02-04 19:59');
-INSERT INTO Submissions VALUES (71, 'A1.pdf', 's4', 4, '2017-02-03 19:59');
+INSERT INTO Submissions VALUES (11, 'A1.pdf', 'A', 1, '2017-02-08 19:59');
+INSERT INTO Submissions VALUES (21, 'A1.pdf', 'B', 1, '2017-02-06 19:59');
+INSERT INTO Submissions VALUES (31, 'A1.pdf', 'A', 1, '2017-02-05 19:59');
+INSERT INTO Submissions VALUES (41, 'A1.pdf', 'B', 1, '2017-02-04 19:59');
+INSERT INTO Submissions VALUES (51, 'A1.pdf', 'A', 1, '2017-02-03 19:59');
+INSERT INTO Submissions VALUES (61, 'A1.pdf', 'C', 4, '2017-02-04 19:59');
+INSERT INTO Submissions VALUES (71, 'A1.pdf', 'D', 4, '2017-02-03 19:59');
 
 -- group id, username
 INSERT INTO Grader VALUES (1, 't1');--
@@ -66,12 +60,12 @@ INSERT INTO Grader VALUES (2, 't2');--
 INSERT INTO Grader VALUES (3, 't1');--
 INSERT INTO Grader VALUES (4, 't4');
 
-INSERT INTO RubricItem VALUES (4010, 1000, 'style', 4, 0.25);
-INSERT INTO RubricItem VALUES (4011, 1000, 'tester', 12, 0.75);--
-INSERT INTO RubricItem VALUES (4020, 2000, 'style', 4, 0.25);
-INSERT INTO RubricItem VALUES (4021, 2000, 'tester', 12, 0.75);
-INSERT INTO RubricItem VALUES (4030, 3000, 'style', 4, 0.25);
-INSERT INTO RubricItem VALUES (4031, 3000, 'tester', 12, 0.75);
+INSERT INTO RubricItem VALUES (4010, 1, 'style', 4, 0.25);
+INSERT INTO RubricItem VALUES (4011, 1, 'tester', 12, 0.75);--
+INSERT INTO RubricItem VALUES (4020, 2, 'style', 4, 0.25);
+INSERT INTO RubricItem VALUES (4021, 2, 'tester', 12, 0.75);
+INSERT INTO RubricItem VALUES (4030, 3, 'style', 4, 0.25);
+INSERT INTO RubricItem VALUES (4031, 3, 'tester', 12, 0.75);
 
 INSERT INTO Grade VALUES (1, 4010, 3);
 INSERT INTO Grade VALUES (1, 4011, 9);--
@@ -80,6 +74,6 @@ INSERT INTO Grade VALUES (2, 4021, 8);
 INSERT INTO Grade VALUES (3, 4030, 1);
 INSERT INTO Grade VALUES (3, 4031, 7);
 
-INSERT INTO Result VALUES (2, 12, true);
+INSERT INTO Result VALUES (2, 8, true);
 INSERT INTO Result VALUES (3, 12, true);
-INSERT INTO Result VALUES (4, 12, true);
+INSERT INTO Result VALUES (4, 7, true), (1, 5, true);
