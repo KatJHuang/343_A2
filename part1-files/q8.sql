a0bbbb1e8f2c70ae4bb6dc5dc3c0ed57ff4029f3
Katherine Huang <kjhuang523@gmail.com>
2017-03-14 00:23:55 -0400

    code for 8; untested

--- part1-files/q8.sql
+++ part1-files/q8.sql
@@ -12,10 +12,93 @@ CREATE TABLE q8 (
 
 -- You may find it convenient to do this for each of the views
 -- that define your intermediate steps.  (But give them better names!)
-DROP VIEW IF EXISTS intermediate_step CASCADE;
+DROP VIEW IF EXISTS groupAssignment CASCADE;
+DROP VIEW IF EXISTS soloAssignment CASCADE;
+DROP VIEW IF EXISTS groups CASCADE;
+DROP VIEW IF EXISTS membersInGroupAssignments CASCADE;
+DROP VIEW IF EXISTS groupAllTheWay CASCADE;
+DROP VIEW IF EXISTS hardWorkersAndWork CASCADE;
+DROP VIEW IF EXISTS hardWorkders CASCADE;
+DROP VIEW IF EXISTS groups CASCADE;
 
 -- Define views for your intermediate steps here.
+
+-- ======================================================================
+-- concerning the assignment percentages for <group, assignment>
+
+create view assignment_outof as
+	select assignment_id, sum(out_of * weight) as assignment_outof
+	from RubricItem group by assignment_id;
+
+create view real_grade as 
+	select assignment_id, group_id, mark * 100/assignment_outof as r_grade
+	from AssignmentGroup natural join assignment_outof natural join Result;
+
+-- ======================================================================
+
+
+-- find all assignments that must be done in groups
+create view groupAssignment as
+	select assignment_id 
+	from Assignment
+	where group_max > 1;
+
+-- find all assignments that must be done solo
+create view soloAssignment as
+	select assignment_id
+	from assignment 
+	where group_max = 1;
+
+-- find groups in those group assignments
+create view groups as
+	select group_id
+	from groupAssignment natural join Membership natural join AssignmentGroup
+	group by group_id
+	having count(username) > 1;
+
+-- find who are in such groups: group id, assignment id, username
+create view membersInGroupAssignments as
+	select group_id, assignment_id, username
+	from groups natural join Membership natural join AssignmentGroup natural join groupAssignment;
+
+	-- 
+create view groupAllTheWay as 
+	select username 
+	from membersInGroupAssignments
+	group by username
+	having count(group_id) >= (select count(distinct assignment_id) from membersInGroupAssignments);
+
+-- find students who submitted in those assignments
+create view hardWorkersAndWork as
+	select username, group_id, assignment_id
+	from submissions natural join membersInGroupAssignments natural join groupAllTheWay
+	group by username, group_id, assignment_id
+	having count(submission_id) > 0;
+
+
+-- find the usernames who never worked solo on an assignment 
+-- that allows groups, and who submitted at least one
+-- file for every assignment
+create view hardWorkers as
+	select distinct username
+	from hardWorkersAndWork;
+
+-- find average mark of groupy people across all group assignments
+create view group_average as
+	select username, avg(r_grade) as group_avg
+	from real_grade natural join hardWorkers natural join groupAssignment
+	group by username;
+
+-- find average mark of groupy people across all group assignments
+create view solo_average as
+	select username, avg(r_grade) as solo_avg
+	from real_grade natural join hardWorkers natural join soloAssignment
+	group by username;
+
+
 
 -- Final answer.
 INSERT INTO q8
 	-- put a final query here so that its results will go into the table.
+	select * 
+	from group_average natural join solo_average;
