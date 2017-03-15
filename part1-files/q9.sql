7a6a14c3acd84c9b09928350609957dbd6c39c86
Katherine Huang <kjhuang523@gmail.com>
2017-03-14 22:40:06 -0400

    added code for q9

--- part1-files/q9.sql
+++ part1-files/q9.sql
@@ -11,10 +11,29 @@ CREATE TABLE q9 (
 
 -- You may find it convenient to do this for each of the views
 -- that define your intermediate steps.  (But give them better names!)
-DROP VIEW IF EXISTS intermediate_step CASCADE;
+DROP VIEW IF EXISTS possiblePairing CASCADE;
+DROP VIEW IF EXISTS allowsGroup CASCADE;
+DROP VIEW IF EXISTS inseparable CASCADE;
 
 -- Define views for your intermediate steps here.
+create view possiblePairing as
+	select m1.username as s1, m2.username as s2
+	from membership as m1, membership as m2
+	where m1.group_id = m2.group_id and m1.username < m2.username;
+
+-- find number of assignments that allows group
+create view allowsGroup as
+	select count(distinct assignment_id) as num_of_assignments
+	from Assignment natural join AssignmentGroup
+	where group_max > 1;
+
+create view inseparable as
+	select * 
+	from possiblePairing
+	group by s1, s2
+	having count(*) > allowsGroup.num_of_assignments;
 
 -- Final answer.
 INSERT INTO q9 
 	-- put a final query here so that its results will go into the table.
+	select * from inseparable;
